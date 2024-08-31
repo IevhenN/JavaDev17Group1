f9a31ac921433542060d3e08b2c00b7d4d7cbe4e
Ievhen <ievhennesterenko@gmail.com>
2024-08-31 13:55:16 +0300

    add emum AccessType, restruct entity note

--- src/main/resources/db/migration/V2__Insert_start_item.sql
+++ src/main/resources/db/migration/V2__Insert_start_item.sql
@@ -1,5 +1,5 @@
-INSERT INTO note (title,content)
-VALUES ('Start note','Start content');
-
 INSERT INTO users (username,password)
 VALUES ('user','$2a$10$feSgAmXnxsmeSysS9CmkEOXhLedNlE9ERZ3UgBlkhS4YA38fCncPO');
+
+INSERT INTO note (id, user_id, title, content, access_type)
+VALUES (UUID(), 1, 'Start note', 'Start content', 'PRIVATE');
