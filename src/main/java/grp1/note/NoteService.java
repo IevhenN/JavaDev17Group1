1887b9902882862237e2c41a5c9cf17f35c77a78
Tatiana Zayarnyuk <tytyshkamalyshka@gmail.com>
2024-09-05 11:38:02 +0300

    note error page

--- src/main/java/grp1/note/NoteService.java
+++ src/main/java/grp1/note/NoteService.java
@@ -10,9 +10,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static java.text.MessageFormat.format;
+
 @Service
 public class NoteService {
     private final NoteRepository noteRepository;
+
+    public static final int NOTE_TITLE_MIN_LENGTH = 5;
+    public static final int NOTE_TITLE_MAX_LENGTH = 100;
+    public static final int NOTE_CONTENT_MIN_LENGTH = 5;
+    public static final int NOTE_CONTENT_MAX_LENGTH = 10000;
 
     @Autowired
     public NoteService(NoteRepository noteRepository) {
@@ -28,8 +35,10 @@ public class NoteService {
     }
 
     public void save(Note note) {
+        validateNote(note);
         noteRepository.save(note);
     }
+
 
     public Optional<Note> getById(String id) {
         return noteRepository.findById(id);
@@ -38,5 +47,15 @@ public class NoteService {
 
     public Optional<Note> getNoteByIdAndUsername(String id, String username) {
         return noteRepository.findByIdAndUsername(id, username);
+    }
+
+    private void validateNote(Note note) {
+        if (note.getTitle().length() < NOTE_TITLE_MIN_LENGTH || note.getTitle().length() >= NOTE_TITLE_MAX_LENGTH) {
+            throw new IllegalArgumentException(format("Note title should be between {0} and {1} characters", NOTE_TITLE_MIN_LENGTH, NOTE_TITLE_MAX_LENGTH));
+        }
+
+        if (note.getContent().length() < NOTE_CONTENT_MIN_LENGTH || note.getContent().length() >= NOTE_CONTENT_MAX_LENGTH) {
+            throw new IllegalArgumentException(format("Note content should be between {0} and {1}  characters", NOTE_CONTENT_MIN_LENGTH, NOTE_CONTENT_MAX_LENGTH));
+        }
     }
 }
